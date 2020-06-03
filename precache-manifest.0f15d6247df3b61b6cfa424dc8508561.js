d14ff161169f2346c1294ab755ae9b7e972e8b84
Nedra Krifa <nedrakrifa@gmail.com>
2020-06-03 17:56:40 +0200

    Updates

---
+++ precache-manifest.0f15d6247df3b61b6cfa424dc8508561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19f4d434fcec5e9d658ceb38dd462c7",
+    "url": "/Pet-Shopping-App/index.html"
+  },
+  {
+    "revision": "8f0b23eea2836f8ad37d",
+    "url": "/Pet-Shopping-App/static/css/main.248abe06.chunk.css"
+  },
+  {
+    "revision": "4ef9befb57c1579f0845",
+    "url": "/Pet-Shopping-App/static/js/2.e52bae99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pet-Shopping-App/static/js/2.e52bae99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0b23eea2836f8ad37d",
+    "url": "/Pet-Shopping-App/static/js/main.bcdc4e0a.chunk.js"
+  },
+  {
+    "revision": "8e82e9bf5ec2a94405db",
+    "url": "/Pet-Shopping-App/static/js/runtime-main.33b7330b.js"
+  }
+]);
